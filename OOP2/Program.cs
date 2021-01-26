db95fa5ecd28fb1834ba2309b4dc6a11e431ceb3
User <User@LAPTOP-L0RDLL3F>
2021-01-26 12:15:15 +0200

    inter lec

---
+++ OOP2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demir";
+            musteri1.TcNo = "12345678910";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "ibo";
+            musteri2.VergiNo = "1234567890";
+
+        }
+    }
+}
